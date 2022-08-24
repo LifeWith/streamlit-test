d6ecab0b9e58d4054f37f62b3061a96fca12ebe9
LifeWith <48851828+LifeWith@users.noreply.github.com>
2022-08-24 17:25:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 import streamlit as st
-import os, sys
-
-@st.experimental_singleton
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-
-_ = installff()
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
-browser.get('http://google.com')
-st.write(browser.page_source)
+st.write('test')
