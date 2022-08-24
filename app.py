f3f8b531f225e2d62fd3b0d0e7b6bc50b57c0d61
LifeWith <48851828+LifeWith@users.noreply.github.com>
2022-08-24 17:57:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 
 options = Options()
-options.add_argument("--headless")
+#options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
+#options.add_argument("--disable-gpu")
 options.add_argument("--disable-features=NetworkService")
 options.add_argument("--window-size=1920x1080")
 options.add_argument("--disable-features=VizDisplayCompositor")
