e3b832c18d0a9425a1aba7431966836272c0d149
LifeWith <48851828+LifeWith@users.noreply.github.com>
2022-08-24 17:50:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import time
+import time, datetime
+import requests
 import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -19,6 +20,34 @@ URL = st.text_input('로그인 할 주소를 입력하세요.')
 SHOP_CODE = st.text_input('로그인 할 샵코드를 입력하세요.')
 ID = st.text_input('로그인할 아이디를 입력하세요.')
 PWD = st.text_input('로그인할 비밀번호를 입력하세요')
+HOOK = st.text_input('알림 받을 슬랙 채널 hook 주소를 입력하세요.')
+
+def send_msg(title:str):
+    now = datetime.datetime.now()
+    nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+    message = '[' + nowDatetime + '] ' + title
+    response = requests.post(
+        HOOK,  
+        headers={
+        'content-type': 'application/json'
+        },
+        json={
+        'text': title,
+        'blocks': [
+            {
+            'type': 'section',
+            'text': {
+                'type': 'mrkdwn',
+                'text': message
+            }
+            }
+        ]
+        }
+    )
+
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+
 
 def Login(driver, url, shopCode, id, pwd):
     try:
@@ -53,4 +82,5 @@ def Run():
     st.write(driver.current_url)
     
 if st.button('TEST'):
-  Run()
+    send_msg('테스트 ')
+    Run()
